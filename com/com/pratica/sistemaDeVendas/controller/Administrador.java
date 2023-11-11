14abb4cf00711d9c7abd63363f535df03dca1617
Doraleous <dioagraleous@gmail.com>
2023-11-11 20:59:35 -0300

    Teste

---
+++ com/com/pratica/sistemaDeVendas/controller/Administrador.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pratica.sistemaDeVendas.controller;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author emart
+ */
+public class Administrador extends Usuario implements Serializable {
+    //teste
+    
+    public Administrador(String CPF, String senha) {
+        super(CPF, senha);
+        
+    }
+    
+}
---
+++ com/com/pratica/sistemaDeVendas/controller/cadastroDeFilmes.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pratica.sistemaDeVendas.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author emart
+ */
+public class cadastroDeFilmes {
+    private ArrayList<Filme> filmes;
+    private String caminhoDoArquivo = "C:\\PraticaProjeto\\filmes";
+    
+    public cadastroDeFilmes() {
+        filmes = this.iniciaCadastro();
+
+    }
+    
+    public void addFilme(String titulo) {
+
+        Filme novoFilme = new Filme(titulo);
+        filmes.add(novoFilme);
+
+    }
+    
+    public void salvaFilmes() {
+        File listaDeFilmes = new File(caminhoDoArquivo);
+
+        try ( ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(listaDeFilmes, false))) {
+            oos.writeObject(filmes);
+            System.out.println("Cadastro saved to " + caminhoDoArquivo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public ArrayList iniciaCadastro() {
+        File listaDeFilmes = new File(caminhoDoArquivo);
+        if (listaDeFilmes.exists()) {
+            try ( ObjectInputStream ois = new ObjectInputStream(new FileInputStream(listaDeFilmes))) {
+                ArrayList<Filme> filmes = (ArrayList<Filme>) ois.readObject();
+                return filmes;
+
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        ArrayList<Filme> filmes = new ArrayList<>();
+        return filmes;
+
+    }
+    
+    public static void main(String args[]) {
+        cadastroDeFilmes cadastro = new cadastroDeFilmes();
+
+        cadastro.addFilme("93492848");
+
+        cadastro.salvaFilmes();
+    }
+    
+    
+    
+}
